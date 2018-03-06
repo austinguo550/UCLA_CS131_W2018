046b1a2f4a530a141f68d3237878b0f72898f4c1
Austin Guo <austinguo550@gmail.com>
2018-03-06 01:16:05 -0800

    Rest of hw4

--- hw4/kenken.pl
+++ hw4/kenken.pl
@@ -1,33 +1,41 @@
 % Kenken using finite domain solver
 % used for inspiration:
 % http://programmablelife.blogspot.com/2012/07/prolog-sudoku-solver-explained.html
+% maplist used to apply curried functions quickly to entire list without explicitly writing out iterations
 
 %%%%% Helper functions
 
-% curried predicate: needed to be able to create curried function taking N applied on Rows
-create_rows(N, Row) :- length(Row, N).
-
 % shapify - create NxN 2D matrix
 shapify(N, T) :-    length(T, N), 
-                    maplist(create_rows(N), T).
+                    create_columns(N, T).
 
+create_columns(N, []). % when there are no more rows to create columns for
+create_columns(N, [H|T]) :- length(H, N), create_columns(N, T). % create columns for each row
+
+% when taking the transpose the objects will still be the same blocks of memory, so mapping fd domains and 
+  % distinct elements on each part will be saved across T and Ts
+% can either take the matrix and return a column and tail, or take in latter 2 and return matrix
+first_column([], [], []).   % base case
+first_column([[H|T] |Tail], [H|Col], [T|Rows]) :- first_column(Tail, Col, Rows).
 % First retrieve first column from tail and set it to first row of NT while retrieving Rest matrix
 % Transpose Rest to NRest and use NRest and the tail T of the first row to populate NTail matrix
-trans([], []).
-trans([ [H|T] | Tail ], [ [H|NT] | NTail ]) :- firstCol(Tail, NT, Rest), trans(Rest, NRest), firstCol(NTail, T, NRest).
-% can either take the matrix and return a column and tail, or take in latter 2 and return matrix
-firstCol([], [], []).
-firstCol([[H|T] |Tail], [H|Col], [T|Rows]) :- firstCol(Tail, Col, Rows).
-
-% curried predicate to restrict populated elements to a be [1, N]
-curried_domain(N, Row) :- fd_domain(Row, 1, N).
-restrict_domain(T, N) :- maplist(curried_domain(N), T).
+trans([], []).    % no more rows and columns to transpose
+trans([ [H|T] | Tail ], [ [H|NT] | NTail ]) :- first_column(Tail, NT, Rest), trans(Rest, NRest), first_column(NTail, T, NRest).
 
 
-decompose_components([X_comp|Y_comp], X_comp, Y_comp).          % pattern match and grab x/y components
-get_value(T, Point, Value) :- decompose_components(Point, I, J), nth1(I, T, Row), nth1(J, Row, Value).  % get the jth element of the ith row
+restrict_domain([], N). % no more elements to restrict domain for
+restrict_domain([Row|T], N) :- fd_domain(Row, 1, N), restrict_domain(T, N). % restrict domain for current row
 
 
+get_value(T, [I|J], Value) :- nth1(I, T, Row), nth1(J, Row, Value).  % get the jth element of the ith row
+
+
+% use maplist because faster, cleaner mapping of fd_all_different and piazza mentions will work better
+fill_with_different(T, Ts) :-   maplist(fd_all_different, T),   % make sure rows are distinct, removes currently mapped row's value from other rows' domains 
+                                maplist(fd_all_different, Ts).  % make sure columns are distinct ... ""
+
+
+% make sure it is true that list elements can add/multiply/subtract/divide and = result given
 apply_constraints(T, C) :- maplist(curried_constraint(T), C).
 
 %% curried constraint predicates
@@ -38,6 +46,7 @@ curried_constraint(T, *(P, L)) :- check_product(T, L, P, 1).
 curried_constraint(T, -(D, J, K)) :- check_difference(T, J, K, D).
 curried_constraint(T, /(Q, J, K)) :- check_quotient(T, J, K, Q).
 
+% TA mentioned need to use #= to account for fd vars manipulation instead of =:= or is (matrix elements are fd vars)
 % sum constraint
 check_sum(_, [], S, S). % recursive base case: compare the sum with the accumulated sum
 check_sum(T, [Point|Other_points], S, Accum) :- get_value(T, Point, Value), Sum #= Accum + Value, check_sum(T, Other_points, S, Sum).
@@ -54,19 +63,18 @@ check_difference(T, J, K, D) :- get_value(T, K, K_val), get_value(T, J, J_val), 
 check_quotient(T, J, K, Q) :- get_value(T, J, J_val), get_value(T, K, K_val), Quotient #= J_val / K_val, Q = Quotient.
 check_quotient(T, J, K, Q) :- get_value(T, J, J_val), get_value(T, K, K_val), Quotient #= K_val / J_val, Q = Quotient.
 
-fill_with_different(T, Ts) :-   maplist(fd_all_different, T),   % make sure rows are distinct, removes currently mapped row's value from other rows' domains    % http://www.gprolog.org/manual/html_node/gprolog062.html#sec325
-                                maplist(fd_all_different, Ts).  % make sure columns are distinct ... ""
 
-% populate the NxN 2D matrix with elements that are within domain range using fd solver
+% populate the NxN 2D matrix with fd vars that are within domain range using fd solver
 populate(N, C, T, Ts) :-    restrict_domain(T, N),          % constrain the Row vars (domain) to values [1,N]
                             fill_with_different(T, Ts),     % make sure the rows and columns have distinct values within domain
                             apply_constraints(T, C),        % apply constraints to matrix
-                            maplist(fd_labeling, T).        % assigns values to row lists in T
+                            maplist(fd_labeling, T).        % assigns final values to row lists in T
 
 
 
 %% kenken function
-
+% similar to sudoku, need to generate empty matrix, get transpose of same matrix in memory, populatee it with distinct elements in fd label
+% extra part is applying extra constraints instead of adding blocks
 kenken(N, C, T) :-  shapify(N, T),              % create unpopulated NxN matrix
                     trans(T, Ts),               % create transform of matrix
                     populate(N, C, T, Ts).      % populate matrix and transposed matrix with constraints and domain
@@ -83,16 +91,16 @@ kenken(N, C, T) :-  shapify(N, T),              % create unpopulated NxN matrix
 form_domain(N, D) :- findall(X, between(1, N, X), D). % populates domain with vars X such that X is in range [1,N]
 
 non_fd_all_different(D, []).
-non_fd_all_different(D, [H|T]) :- member(H, D), non_fd_all_different(D, T), \+member(H, T).
+non_fd_all_different(D, [H|T]) :- member(H, D), non_fd_all_different(D, T), \+member(H, T).   % make sure each row element is in domain and not in rest of row
 
 
-plain_fill_with_different(D, T, Ts) :-  maplist(non_fd_all_different(D), T),
-                                        maplist(non_fd_all_different(D), Ts).
+plain_fill_with_different(D, T, Ts) :-  maplist(non_fd_all_different(D), T),    % make sure rows have different elements
+                                        maplist(non_fd_all_different(D), Ts).   % make sure columns have different elements
 
 
-apply_plain_constraints(T, C) :- maplist(plain_curried_constraint(T), C).
+apply_plain_constraints(T, C) :- maplist(plain_curried_constraint(T), C).     % apply plain curried constraints
 
-%% curried constraint predicates
+%% curried constraint predicates for plain kenken (is used instead of #=, according to TA since #= used for fd solver vars only)
 
 % apply numeric cage constraints to each element in the list of constraints using pattern matching
 plain_curried_constraint(T, +(S, L)) :- plain_check_sum(T, L, S, 0).
@@ -116,7 +124,6 @@ plain_check_difference(T, J, K, D) :- get_value(T, K, K_val), get_value(T, J, J_
 plain_check_quotient(T, J, K, Q) :- get_value(T, J, J_val), get_value(T, K, K_val), Quotient is J_val / K_val, Q = Quotient.
 plain_check_quotient(T, J, K, Q) :- get_value(T, J, J_val), get_value(T, K, K_val), Quotient is K_val / J_val, Q = Quotient.
 
-%  not(member(H, T)), non_fd_all_different(T).
 
 plain_populate(N, C, T, Ts) :-  form_domain(N, D),  % elements of T must be part of domain D
                                 plain_fill_with_different(D, T, Ts),
@@ -127,9 +134,11 @@ plain_kenken(N, C, T) :-    shapify(N, T),
                             trans(T, Ts),
                             plain_populate(N, C, T, Ts).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
+% No-op kenken is in the file noopkenken.txt
+% Has no implementation: only contains API and example call
 
 kenken_testcase(
     6,
@@ -151,3 +160,26 @@ kenken_testcase(
      /(2, [6|4], [6|5])
     ]
   ).
+
+
+no_op_kenken_testcase(
+    3,
+    [
+      *(6, [[1|2], [2|1]]),
+      +(2, [[2|2], [3|3]]),
+      +(3, [[1|1], [1|3]])
+    ]
+  ).
+
+kenken(
+  4,
+  [
+   +(6, [[1|1], [1|2], [2|1]]),
+   *(96, [[1|3], [1|4], [2|2], [2|3], [2|4]]),
+   -(1, [3|1], [3|2]),
+   -(1, [4|1], [4|2]),
+   +(8, [[3|3], [4|3], [4|4]]),
+   *(2, [[3|4]])
+  ],
+  T
+), write(T), nl, fail.
